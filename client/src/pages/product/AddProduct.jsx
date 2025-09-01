9995066f6ea5d570d22dd953298347844a58ee09
Red0220 <stevenkin817@gmail.com>
2025-09-02 01:17:25 +0200

    i

--- client/src/pages/product/AddProduct.jsx
+++ client/src/pages/product/AddProduct.jsx
@@ -10,14 +10,16 @@ const FormField = ({ label, children, dir='col' })=>
  className={`flex ${dir==='row' ? 'flex-row  items-center border border-gray-200 p-2 rounded-md ' : 'flex-col'} gap-3`}>
   <span 
   className={`text-sm font-medium text-slate-600 whitespace-nowrap ${dir === 'row' ? 'px-2 border-r border-inherit min-w-[60px]': ''}`}>{label} :</span>
-  
       {children}
-  
-
  </label>
 
 
+const discountCalculator = (price, discount) => {
+  if (!discount) return price;
+  return price - (price * discount) / 100;
+}
 
+console.log(discountCalculator(98, 20)); // 80
 
 
 const AddProduct = () => {
@@ -29,7 +31,7 @@ const AddProduct = () => {
 
   console.log(user);
 
-
+    
 
 
   return (
@@ -66,7 +68,7 @@ const AddProduct = () => {
           <h1 className="text-lg font-medium text-slate-800 ">
            Price & Details
           </h1>
-           <div className="flex flex-col md:flex-row gap-6">
+           <div className="flex flex-col md:flex-row gap-6 text-xs">
               <FormField label={'Price'} dir="row">
               <input 
                 type="number"
@@ -78,16 +80,16 @@ const AddProduct = () => {
            <FormField label={'Discount (%)'} dir="row">
               <input 
               type='number'
-              className={`${baseInput} w-[4rem]`}/>
+              className={`${baseInput} w-[3rem]`}/>
            </FormField>
-               {/* <FormField label={'Price'} dir="row">
+               <FormField label={'Price'} dir="row">
               <input 
                 type="number"
                 placeholder="Price"
                 min={0}
-                className={`${baseInput}  `}
+                className={`${baseInput} `}
               />
-          </FormField> */}
+          </FormField>
 
            </div>
         </div>
@@ -96,8 +98,35 @@ const AddProduct = () => {
           <h1 className="text-lg font-medium text-slate-800 pb-2 ">
             Add a new product
           </h1>
-          <input type='number'  placeholder='discount'
-          className={`${baseInput} `} />
+         <label
+        htmlFor="image"
+        className="flex h-40 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-100 hover:bg-gray-100"
+      >
+        <div className="flex flex-col items-center justify-center pb-6 pt-5">
+          <svg
+            className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 16"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+            />
+          </svg>
+          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+            <span className="font-semibold">Click to upload</span> or drag and drop
+          </p>
+          <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+        </div>
+        <input type='file' id="image"  accept='image/*' multiple 
+        onChange={(e)=>setFiles(e.target.files)}
+     hidden />
+      </label>
         </div>
            
         {/* submit button */}
